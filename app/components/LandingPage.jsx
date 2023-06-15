6a7f6176cb082a3200af40c36f2a699144281a8f
Shubham Gaikwad <99805248+sghere@users.noreply.github.com>
2023-06-15 09:46:06 +0530

    Animations and responsive font sizes added

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -3,17 +3,32 @@ import Intro from "./Intro";
 import LinksFloater from "./LinksFloater";
 import TechStack from "./TechStack";
 import GetInTouch from "./GetInTouch";
+import "./BackgroundAnimation.css";
 
 const LandingPage = () => {
   return (
     <>
-      <LinksFloater />
-      <Intro />
-      <TechStack Type="intro" />
-      <TechStack />
-      <TechStack Type="opensource / contributions" />
-      <TechStack Type="projects" />
-      <GetInTouch />
+      <div className="Landing">
+        <LinksFloater />
+        <Intro />
+        <TechStack Type="intro" />
+        <TechStack />
+        <TechStack Type="opensource / contributions" />
+        <TechStack Type="projects" />
+        <GetInTouch />
+      </div>
+      <ul class="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </>
   );
 };
