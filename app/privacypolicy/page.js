0d6c73358f7c6b01de89076338d923c6b90512cb
Shubham Gaikwad <99805248+sghere@users.noreply.github.com>
2023-11-09 13:33:38 +0530

    privacypolicy pushed

---
+++ app/privacypolicy/page.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <header>
+        <h1>Privacy Policy</h1>
+      </header>
+
+      <main>
+        <p>
+          Welcome to Finder by ByteCtrl ("we," "our," or "us"). At Finder by
+          ByteCtrl, we are committed to protecting your privacy and ensuring the
+          security of your personal information. This Privacy Policy outlines
+          how we collect, use, disclose, and safeguard your data when you use
+          our product.
+        </p>
+        --- **Privacy Policy for Finder by ByteCtrl** **1. Introduction**
+        Welcome to Finder by ByteCtrl ("we," "our," or "us"). At Finder by
+        ByteCtrl, we are committed to protecting your privacy and ensuring the
+        security of your personal information. This Privacy Policy outlines how
+        we collect, use, disclose, and safeguard your data when you use our
+        product. **2. Information We Collect** We may collect the following
+        types of information when you use Finder by ByteCtrl: - Personal
+        Information: This includes information you provide when creating an
+        account, such as your name, email address, and LinkedIn credentials. -
+        Usage Information: We may collect data on how you use our product,
+        including your interactions and activities within the application. -
+        Communications: Any communications you have with us, including emails
+        and messages, may be stored for record-keeping and support purposes.
+        **3. How We Use Your Information** We use your information for the
+        following purposes: - To provide, maintain, and improve Finder by
+        ByteCtrl. - To personalize your experience and deliver content tailored
+        to your preferences. - To communicate with you, respond to your
+        inquiries, and provide customer support. - To comply with legal
+        obligations. **4. Data Security** We take reasonable measures to protect
+        your data from unauthorized access, disclosure, or alteration. However,
+        no data transmission over the internet is completely secure, and we
+        cannot guarantee the security of your information. **5. Sharing Your
+        Information** We do not share your personal information with third
+        parties except as described in this Privacy Policy or with your consent.
+        We may share your data with service providers and business partners who
+        assist us in delivering and improving our product. **6. Your Choices**
+        You can access and update your personal information through your account
+        settings. You may also opt-out of receiving promotional communications
+        from us. **7. Children's Privacy** Finder by ByteCtrl is not intended
+        for use by children under the age of 13. We do not knowingly collect
+        personal information from children. **8. Changes to this Privacy
+        Policy** We may update this Privacy Policy to reflect changes in our
+        data practices. We will notify you of any significant updates through
+        our product or via email. **9. Contact Us** If you have any questions or
+        concerns about this Privacy Policy or your personal data, please contact
+        us at [contact email]. --- Please remember that this is a basic template
+        and may need to be customized to accurately reflect your product's
+        specific data practices and legal requirements. It's essential to seek
+        legal advice to ensure that your privacy policy complies with applicable
+        laws and regulations.
+      </main>
+
+      <footer>
+        <p>Contact Us: [contact email]</p>
+      </footer>
+    </div>
+  );
+};
+
+export default page;
